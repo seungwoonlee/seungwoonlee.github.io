beb398d1cd326bc630ad8ad2678ce73430515dca
seungwoonlee <bluecrck@gmail.com>
2026-03-17 20:03:26 +0900

    feat: Astro 블로그 초기화 및 배포 브랜치 설정(master)

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Vibe Coding: seungwoonlee\'s Blog';
+export const SITE_DESCRIPTION = '바이브 코딩과 기술적 노하우를 기록하는 개발용 개인 블로그';
